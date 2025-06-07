189a1c441a6d8fb30a8dd96e54b719966c6e9808
Aderojuola Ayomide <aderojuolaayomide@Aderojuolas-MacBook-Pro.local>
2025-06-07 20:21:25 +0100

    Home Css design

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -21,15 +21,9 @@ function AboutUs() {
                     <h2 className="about-us-title">ABOUT BRICKSPOINT BOUTIQUE APARTHOTEL ASOKORO</h2>
                     <h3 className="about-us-subtitle">YOUR SMART CHOICE IN ABUJA, NIGERIA</h3>
                     <p className="about-us-description">
-                        Indulge in luxury at Brickspoint Boutique ApartHotel, nestled in Asokoro, Abuja. Immerse
-                        yourself in chic comfort just moments from the CBD. Enjoy personalized service in a vibrant
-                        locale near embassies, ensuring an unforgettable stay.
+                       Indulge in luxury at Brickspoint Boutique ApartHotel, nestled in Asokoro, Abuja. Immerse yourself in chic comfort just moments from the CBD. Enjoy personalized service in a vibrant local near embassies, ensuring an unforgettable stay. 
                     </p>
-                    <p className="about-us-footer-text">
-                        Enjoy modern amenities, including high-speed WIFI and state-of-the-art systems. The spacious
-                        and thoughtfully arranged interiors provide a serene retreat, while our large windows offer
-                        natural daylight and a large terrace.
-                    </p>
+            
                     <button className="about-us-book-now">BOOK NOW</button>
                 </div>
                 <div className="about-us-image-container">
